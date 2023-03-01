2a81eb7219a1e4b5294e8ed52f021592878c838b
Mathew Chandler <Cascabelrock@gmail.com>
2023-02-28 17:33:45 -0800

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Page from "./components/Page";
+import Footer from "./components/Footer";
+
+function App() {
+  const [pages] = useState([
+    { name: "About Me" },
+    { name: "Portfolio" },
+    { name: "Contact" },
+    { name: "Resume" },
+  ]);
+  // the usestate hook allows us to keep track of the current page and also set the value of current page
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  return (
+    <div>
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
